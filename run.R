c06f89c0e9c7da3957b3cd6a805fdedba0cb9f5f
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-30 01:10:23 +0900

    Update run.R

--- run.R
+++ run.R
@@ -1,11 +1,9 @@
-library(prophet)
-library(rvest)
-require(RCurl)
+library(shiny)
 
-fileurl <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSiFo4cr-L9HNgQAOJdbHTZmE16VnYjcJ3DZYneS13fQ0W4W-kfh2pV2WTnc7FtBILtVGuq9w1eOCxb/pub?gid=51203032&single=true&output=csv"
-fileCSV <- getURL(fileurl,.opts=list(ssl.verifypeer=FALSE))
-df <-  read.csv(textConnection(fileCSV),header=TRUE)
-m <- prophet(df)
-future <- make_future_dataframe(m, periods = 120)
-forecast <- predict(m, future)
-plot(m, forecast)+theme_bw()
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
