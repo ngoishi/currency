fd3bed5789e226603d4d899fc82515842f6af4ad
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-30 03:01:37 +0900

    Update server.R

--- server.R
+++ server.R
@@ -6,13 +6,12 @@ library(data.table)
 function(input, output) {
   output$plot1 <- renderPlot({
     
-    fileurl <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSiFo4cr-L9HNgQAOJdbHTZmE16VnYjcJ3DZYneS13fQ0W4W-kfh2pV2WTnc7FtBILtVGuq9w1eOCxb/pub?gid=51203032&single=true&output=csv"
+    fileurl <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSiFo4cr-L9HNgQAOJdbHTZmE16VnYjcJ3DZYneS13fQ0W4W-kfh2pV2WTnc7FtBILtVGuq9w1eOCxb/pub?gid=0&single=true&output=csv"
     fileCSV <- getURL(fileurl,.opts=list(ssl.verifypeer=FALSE))
     df <-  read.csv(textConnection(fileCSV),header=TRUE)
-    mydf <- data.frame(df[,2])
-    mydf <- copy(mydf)
-    setDT(mydf, keep.rownames = TRUE)
-    colnames(mydf)<- c("ds", "y")
+    
+    df[,1]<-as.Date(df[,1], format = "%m/%d/%Y")
+    colnames(df)<- c("ds", "y")
     m <- prophet(df)
     future <- make_future_dataframe(m, periods = input$forward*30)
     forecast <- predict(m, future)
