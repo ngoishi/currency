82cfc493aa626a618cd9fe6a212fca23131db2de
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-30 01:51:14 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 fluidPage(
-  titlePanel("Playing with Prophet on Financial Time Series"),
+  titlePanel("Exchange Rate Forecasts"),
   fluidRow(
+    sliderInput("forward", "Forecast window size (months):", min = 1, max = 12, value = 6, step = 1),
            plotOutput("plot1", width = 1200, height = 900)
   )
 )
