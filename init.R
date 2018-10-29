63b9a226721999fe442bf2b144bac726052cc1eb
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-30 01:11:00 +0900

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("prophet","RCurl","rvest")
+my_packages = c("prophet","RCurl","rvest","data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
